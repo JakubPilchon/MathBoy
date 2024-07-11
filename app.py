38a8a07811f3a268f117fca32a2c04aedc31c956
JakubPilchon <jakubpilchon@gmail.com>
2024-07-11 20:21:26 +0200

    added reset button

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ class Applicaion(tk.Tk):
 
     def __stop_paint(self, event):
         self.is_painting = False
+    
+    def reset(self):
+        self.canvas.delete("all")
 
     def __init__(self):
         super().__init__()
@@ -41,11 +44,14 @@ class Applicaion(tk.Tk):
         tk.Radiobutton(colorframe, variable = self.color, value="blue",  selectcolor="#0000ff", cursor="hand2").grid(column=2, row=0, sticky="NW")
         tk.Radiobutton(colorframe, variable = self.color, value="yellow",selectcolor="#ffee03", cursor="hand2").grid(column=2, row=1, sticky="NW")
 
-        tk.Radiobutton(colorframe, variable = self.color, value="violet", selectcolor="#8503ff", cursor="hand2").grid(column=3, row=0, sticky="NW")
+        tk.Radiobutton(colorframe, variable = self.color, value="purple", selectcolor="#8503ff", cursor="hand2").grid(column=3, row=0, sticky="NW")
         tk.Radiobutton(colorframe, variable = self.color, value="pink",   selectcolor="#ff03f2", cursor="hand2").grid(column=3, row=1, sticky="NW")
 
         button = ttk.Button(colorframe, text="Click button", command=self.__button_click)
         button.grid(column=4, row=1, sticky='NW')
+
+        reset_button = ttk.Button(colorframe, text="reset", command=self.reset)
+        reset_button.grid(column=4, row=0, sticky='NW')
 
         self.canvas = tk.Canvas(self, width=1250, height=700, background="#ffffff", cursor="plus")
         self.canvas.grid(column=0, row=2)
